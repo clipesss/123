813405cb0fe523e92706cb1fa5c7914d9df750ba
Ilya <100891851+clipesss@users.noreply.github.com>
2025-10-05 15:48:57 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  const buttons = document.querySelectorAll('.cta');
-  buttons.forEach(btn => {
-    if (isMobile) {
-      btn.setAttribute('href', 'tel:+375333636898');
-    }
-  });
-});
